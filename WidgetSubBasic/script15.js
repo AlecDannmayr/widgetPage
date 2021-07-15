ca68649603256961f3b9adcc028fe90328010313
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-07-15 12:11:27 +0100

    --- Auto-Git Commit ---

--- WidgetSubBasic/script15.js
+++ WidgetSubBasic/script15.js
@@ -560,14 +560,49 @@ function matchRun(idNo) {
                   coms[k].innerHTML = cint + '.' + crtbl + '  '
                } else {
                   console.log(covb)
-
+                  let covb0, covb1, covb2, covb3, covb4, covb5
+                  switch (covb) {
+                     case 0:
+                        covb0 = 1
+                        break
+                     case 1:
+                        covb0 = 2
+                        covb1 = 1
+                        break
+                     case 2:
+                        covb0 = 3
+                        covb1 = 2
+                        covb2 = 1
+                        break
+                     case 3:
+                        covb0 = 4
+                        covb1 = 3
+                        covb2 = 2
+                        covb3 = 1
+                        break
+                     case 4:
+                        covb0 = 5
+                        covb1 = 4
+                        covb2 = 3
+                        covb3 = 2
+                        covb3 = 1
+                        break
+                     case 5:
+                        covb0 = 6
+                        covb1 = 5
+                        covb2 = 4
+                        covb3 = 3
+                        covb3 = 2
+                        covb3 = 1
+                        break
+                  }
                   const cmlg = covb.length
-                  coms[0].innerHTML = int + '.' + covb + '  ' + '| ' + commentaries[0].split('-')[0]
-                  coms[1].innerHTML = int + '.' + covb + '  ' + '| ' + commentaries[1].split('-')[0]
-                  coms[2].innerHTML = int + '.' + covb + '  ' + '| ' + commentaries[2].split('-')[0]
-                  coms[3].innerHTML = int + '.' + covb + '  ' + '| ' + commentaries[3].split('-')[0]
-                  coms[4].innerHTML = int + '.' + covb + '  ' + '| ' + commentaries[4].split('-')[0]
-                  coms[5].innerHTML = int + '.' + covb + '  ' + '| ' + commentaries[5].split('-')[0]
+                  coms[0].innerHTML = int + '.' + covb0 + '  ' + '| ' + commentaries[0].split('-')[0]
+                  coms[1].innerHTML = int + '.' + covb1 + '  ' + '| ' + commentaries[1].split('-')[0]
+                  coms[2].innerHTML = int + '.' + covb2 + '  ' + '| ' + commentaries[2].split('-')[0]
+                  coms[3].innerHTML = int + '.' + covb3 + '  ' + '| ' + commentaries[3].split('-')[0]
+                  coms[4].innerHTML = int + '.' + covb4 + '  ' + '| ' + commentaries[4].split('-')[0]
+                  coms[5].innerHTML = int + '.' + covb5 + '  ' + '| ' + commentaries[5].split('-')[0]
                }
             }
 
