232c37d8ad79e474584d77900323ed5404617407
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-07-22 12:30:28 +0100

    --- Auto-Git Commit ---

--- script11.js
+++ script11.js
@@ -126,10 +126,9 @@ window.addEventListener('load', function () {
          for (itl = 0; itl < r.length; itl++) {
             if (r[itl].start.substring(0, 10) == nuDt) {
                fixt = r[itl].start.split('T')[1].substring(0, 5) + '&nbsp&nbsp'
-            }
-            for (gt = 0; gt < ftxt.length; gt++) {
-               ftxt[gt].innerHTML = r[itl].start.split('T')[1].substring(0, 5) + '&nbsp&nbsp'
-               console.log(fixt)
+               for (gt = 0; gt < ftxt.length; gt++) {
+                  console.log(r[itl].start.split('T')[1].substring(0, 5) + '&nbsp&nbsp')
+               }
             }
          }
 
