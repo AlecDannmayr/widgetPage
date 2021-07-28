2da38cf1d8915f836b42cd310cb1a921b0042aa2
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-07-28 17:39:51 +0100

    --- Auto-Git Commit ---

--- WidgetSubBasic/script_v23.js
+++ WidgetSubBasic/script_v23.js
@@ -494,7 +494,7 @@ socket.onmessage = function (event) {
            // if (inns2 != null) {
                 for (i = 0; i < b1sn2.length; i++) {
                    b1sn2[i].innerHTML = inns1.batting[i].name
-                   if (t1b[i].textContent == '') {
+                   if (b1sn2[i].textContent == '') {
                       b1sn2[i].style.display = 'none'
                       b1st2[i].style.display = 'none'
                       b1b2[i].style.display = 'none'
@@ -502,27 +502,29 @@ socket.onmessage = function (event) {
                       b14s2[i].style.display = 'none'
                       b16s2[i].style.display = 'none'
                    } else {
-                       b1sn2[i].style.display = 'block'
-                       b1st2[i].style.display = 'block'
-                       b1b2[i].style.display = 'block'
-                       b1r2[i].style.display = 'block'
-                       b14s2[i].style.display = 'block'
-                       b16s2[i].style.display = 'block'
+                      b1sn2[i].style.display = 'block'
+                      b1st2[i].style.display = 'block'
+                      b1b2[i].style.display = 'block'
+                      b1r2[i].style.display = 'block'
+                      b14s2[i].style.display = 'block'
+                      b16s2[i].style.display = 'block'
                    }
                 }
 
             for (i = 0; i < b1sn2.length; i++) {
                b1sn2[i].innerHTML = inns1.bowling[i].name
                if (inns1.bowling[i].name == '') {
-                  t2bnb[i].style.display = 'none'
-                  t2bo[i].style.display = 'none'
-                  t2br[i].style.display = 'none'
-                  t2bw2[i].style.display = 'none'
+                  t2bnb2[i].style.display = 'none'
+                  t2bo2[i].style.display = 'none'
+                  t2br2[i].style.display = 'none'
+                  t2bw22[i].style.display = 'none'
+                  t2bw222[i].style.display = 'none'
                } else {
                   t2bnb[i].style.display = 'block'
                   t2bo[i].style.display = 'block'
                   t2br[i].style.display = 'block'
                   t2bw2[i].style.display = 'block'
+                  t2bw222[i].style.display = 'block'
                }
             }
 
