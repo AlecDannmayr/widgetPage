552037ff67196401a80806442822429293a520d8
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-08-17 10:30:37 +0100

    --- Auto-Git Commit ---

--- WidgetSubBasic/script_v28.js
+++ WidgetSubBasic/script_v28.js
@@ -423,7 +423,7 @@ socket.onmessage = function (event) {
             }, 0)
 
          ex1[0].innerText = addext
-         tl1[0].innerText = t1fis.textContent.replace(' / ', '') + ' (' + t1fw.textContent + ' Wkts' + ')'
+         tl1[0].innerText = t1fis.textContent.replace(' / ', '') + '  ' + ' ( ' + t1fw.textContent + ' wkts' + ' ) '
 
          for (i = 0; i < t1b.length; i++) {
             t1b[i].innerHTML = ino3.batting[i].name
